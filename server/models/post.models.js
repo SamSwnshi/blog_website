15be562c6c22d0e009dd2259e1bcb1feced49faf
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-07-26 15:43:11 +0530

    post and comment models

---
+++ server/models/post.models.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true },
+        content: { type: String, required: true },
+        image: { type: String },
+        tags: [{ type: String }],
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    },
+    { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
