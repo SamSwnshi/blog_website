48fa0b78212971a3731ad466d79925c485db0870
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-07-27 20:48:13 +0530

    dashboard

--- client/src/component/Dashboard.jsx
+++ client/src/component/Dashboard.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <div >
-      <div>
-
+      <div className='flex justify-between px-12 py-6'>
+        <h1 className='text-2xl font-bold'>My Posts</h1>
+        <button className=' p-2 rounded-md bg-purple-400'>+ Create Post</button>
       </div>
+
     </div>
   )
 }
