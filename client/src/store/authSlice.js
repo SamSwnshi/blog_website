8acf203ecb88d250ef47daf4c5b410f570740cd1
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-07-28 15:39:04 +0530

    authslice

---
+++ client/src/store/authSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  token: localStorage.getItem("jwt") || null,
+  loading: false,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    startLoading(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    loginSuccess(state, action) {
+      state.loading = true;
+      state.token = action.payload;
+      state.error = null;
+    },
+    loginFailure(state, action) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    logout(state) {
+      state.token = null;
+      state.loading = false;
+      state.error = null;
+      localStorage.removeItem("jwt");
+    },
+  },
+});
+
+export const {startLoading,loginSuccess,loginFailure,logout} = authSlice.actions;
+
+export default authSlice.reducer;
