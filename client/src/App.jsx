e5dabe350af71ff9150b1a2b82f140d2660ee2a7
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-07-28 15:04:31 +0530

    home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,13 @@ import Register from './pages/Register'
 import Login from './pages/Login'
 import Dashboard from './component/Dashboard'
 import CreatePost from './pages/CreatePost'
+import Home from './component/Home'
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes>
+        <Route path='/'element={<Home/>}>Home</Route>
         <Route path='/dashboard'element={<Dashboard/>}>Dashboard</Route>
         <Route path='/login' element={<Login/>}>Login</Route>
         <Route path='/signup'element={<Register/>}>Register</Route>
